3ccfdc38f5a93fb9d914fdc3f010b0bc0112d237
Brennan Shacklett <bpshacklett@gmail.com>
2021-01-26 15:41:49 -0800

    Preliminary support for Habitat's JSON scene format

---
+++ src/habitat_json.cpp
@@ -0,0 +1,19 @@
+#include "habitat_json.hpp"
+
+#include <filesystem>
+
+using namespace std;
+
+namespace RLpbr {
+namespace SceneImport {
+
+HabitatJSONScene habitatJSONLoad(string_view scene_path)
+{
+    (void)scene_path;
+
+    return HabitatJSONScene {
+    };
+}
+
+}
+}
