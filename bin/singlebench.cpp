6796b570500dd2aa95387480f0a897211ed5bad8
Brennan Shacklett <bpshacklett@gmail.com>
2021-05-03 20:22:01 -0700

    Merge in opt branch

--- bin/singlebench.cpp
+++ bin/singlebench.cpp
@@ -4,54 +4,59 @@
 #include <chrono>
 #include <fstream>
 
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/string_cast.hpp>
+
+#define STRINGIFY_HELPER(m) #m
+#define STRINGIFY(m) STRINGIFY_HELPER(m)
+
+
 using namespace std;
 using namespace RLpbr;
 
-constexpr uint32_t num_frames = 1000000;
+constexpr uint32_t num_frames = 100000;
 
-constexpr uint32_t max_load_frames = 10000;
-
-vector<glm::mat4> readViews(const char *dump_path)
+vector<pair<glm::vec3, glm::quat>> readViews(const string &dump_path)
 {
     ifstream dump_file(dump_path, ios::binary);
+    uint32_t num_views;
+    dump_file.read((char *)&num_views, sizeof(uint32_t));
 
-    vector<glm::mat4> views;
+    vector<pair<glm::vec3, glm::quat>> views;
 
-    for (size_t i = 0; i < max_load_frames; i++) {
-        float raw[16];
-        dump_file.read((char *)raw, sizeof(float)*16);
-        views.emplace_back(
-                glm::mat4(raw[0], raw[1], raw[2], raw[3],
-                          raw[4], raw[5], raw[6], raw[7],
-                          raw[8], raw[9], raw[10], raw[11],
-                          raw[12], raw[13], raw[14], raw[15]));
+    for (int i = 0; i < (int)num_views; i++) {
+        glm::vec3 position;
+        glm::quat rotation;
+        dump_file.read((char *)glm::value_ptr(position), sizeof(glm::vec3));
+        dump_file.read((char *)glm::value_ptr(rotation), sizeof(glm::quat));
+
+        views.emplace_back(position, rotation);
     }
 
     return views;
 }
 
 int main(int argc, char *argv[]) {
-    if (argc < 4) {
-        cerr << argv[0] << " scene batch_size res views" << endl;
+    if (argc < 6) {
+        cerr << argv[0] << " scene batch_size res spp path depth views" << endl;
         exit(EXIT_FAILURE);
     }
 
     uint32_t batch_size = stoul(argv[2]);
     uint32_t res = stoul(argv[3]);
-    vector<glm::mat4> init_views;
-    if (argc > 4) {
-        init_views = readViews(argv[4]);
+    uint32_t spp = stoul(argv[4]);
+    uint32_t path_depth = stoul(argv[5]);
+    vector<pair<glm::vec3, glm::quat>> views;
+    if (argc > 6) {
+        views = readViews(argv[6]);
     } else {
-        init_views = {glm::mat4(
-            -1.19209e-07, 0, 1, 0,
-            0, 1, 0, 0,
-            -1, 0, -1.19209e-07, 0,
-            -3.38921, 1.62114, -3.34509, 1)
-        };
+        views = readViews(string(STRINGIFY(RLPBR_DATA_DIR)) + "/test_cams.bin");
     }
 
-    Renderer renderer({0, 1, batch_size, res, res, 4, 4, false, false,
-                       0, BackendSelect::Optix});
+    cout << "Loaded " << views.size() << " views" << endl;
+
+    Renderer renderer({0, 1, batch_size, res, res, spp, path_depth, 1, false,
+        false, BackendSelect::Optix});
 
     auto loader = renderer.makeLoader();
     auto scene = loader.loadScene(argv[1]);
@@ -60,21 +65,26 @@ int main(int argc, char *argv[]) {
 
     for (uint32_t batch_idx = 0; batch_idx < batch_size; batch_idx++) {
         envs.emplace_back(renderer.makeEnvironment(scene,
-            glm::vec3(10.184777, 0.994812, -2.762561),
-            glm::vec3(10.812518, 0.927049, -1.987069),
-            glm::vec3(-0.000107, 0.996203, 0.087133)));
+            glm::vec3(0.f), glm::vec3(0.f, 0.f, 1.f),
+            glm::vec3(0.f, 1.f, 0.f), glm::vec3(1.f, 0.f, 0.f)));
     }
 
     auto start = chrono::steady_clock::now();
 
     uint32_t num_iters = num_frames / batch_size;
 
-    //uint32_t cur_view = 0;
+    uint32_t cur_view = 0;
 
     for (uint32_t i = 0; i < num_iters; i++) {
-        //for (auto &env : envs) {
-        //    env.setDirty();
-        //}
+        for (auto &env : envs) {
+            auto [position, rotation] = views[cur_view];
+            cur_view = (cur_view + 1) % views.size();
+
+            env.setCameraView(position,
+                rotation * glm::vec3(0.f, 0.f, 1.f),
+                glm::vec3(0.f, 1.f, 0.f),
+                rotation * glm::vec3(1.f, 0.f, 0.f));
+        }
         renderer.render(envs.data());
         renderer.waitForFrame();
     }
