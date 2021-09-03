cb6a0a4ef549e7c1bcdeb9cba85178241c06ca92
Brennan Shacklett <bpshacklett@gmail.com>
2021-09-03 12:24:46 -0700

    Minor bugfix in renderbatch

--- include/rlpbr/render.hpp
+++ include/rlpbr/render.hpp
@@ -18,6 +18,11 @@ public:
 
     RenderBatch(Handle &&backend,  std::vector<Environment> &&envs);
 
+    inline void addEnvironment(Environment &&env)
+    { 
+        envs_.emplace_back(std::move(env));
+    }
+
     inline Environment &getEnvironment(uint32_t idx) { return envs_[idx]; }
     inline Environment *getEnvironments() { return envs_.data(); }
 
